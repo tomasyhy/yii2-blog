465a90e9bf73eb6408a60804c9d4d04a294cd76c
Tomasz <tomasz.hliwiadczyn@opegieka.pl>
2016-12-23 15:55:17 +0100

    configuration pretty url

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -14,6 +14,7 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
+            'baseUrl' => '',
         ],
         'user' => [
             'identityClass' => 'common\models\User',
@@ -36,14 +37,10 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
         ],
-        */
     ],
     'params' => $params,
 ];
